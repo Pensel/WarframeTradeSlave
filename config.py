a983aba660aff16b379964680c5a553b83a13f61
Pensel <johannes.p.pensel@gmx.de>
2018-07-25 19:06:41 +0200

    first

---
+++ config.py
@@ -0,0 +1,19 @@
+before = "WTS"
+
+preisMit = {'Veiled Melee Riven' : 28,
+            'Veiled Pistol Riven' : 28}
+
+#:platinum: geht auch
+platinum = "p"
+
+
+#niemals shell operatoren verwenden ( && oder || oder | ) 
+seperator = "  "
+
+preisOhne = {'[Grakata Feva-satisus]',
+              '[Mutalist Quanta Satipha]',
+              '[Panthera Sci-mantitox]'}
+
+after = "PMO"
+
+
---
+++ sellCB.py
@@ -0,0 +1,51 @@
+import subprocess
+from pyautogui import press, typewrite, hotkey, click
+from time import sleep
+import config
+import WindowMgr
+
+
+def wrapper(mit, ohne, before, after, seperator, platinum):
+    seperator = cleanSep(seperator)
+    if len(mit) == 0:
+        sellString =  withoutPrice(ohne)
+    elif len(ohne) == 0:
+        sellString = withPrice(mit, platinum)
+    else:
+        sellString = withPrice(mit, platinum)  + seperator + withoutPrice(ohne)
+
+    return 'echo '+ before +" " + sellString + " " + after + ' |clip'
+
+
+def cleanSep(sep):
+    return sep.replace("|", " ")
+
+def withPrice(d, platinum):
+    return "".join([(k + " " + str(d[k]) + platinum) for k in d])
+
+def withoutPrice(l):
+    return ''.join(l)
+
+def sell(text):
+    return subprocess.check_call(text, shell=True)
+
+def swapWindow():
+    w = WindowMgr.WindowMgr()
+    w.find_window_wildcard("WARFRAME")
+    w.set_foreground()
+    click()
+    
+
+def autoPrint(s=0):
+    sleep(s)
+    press('T')
+    sleep(.333)
+    hotkey('ctrl','v')
+    press('enter')
+
+if __name__ == '__main__':
+    sell(wrapper(config.preisMit, config.preisOhne , config.before, config.after, config.seperator, config.platinum))
+##    swapWindows()
+##    autoPrint(2)
+
+
